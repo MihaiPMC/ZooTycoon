fd21d7a6f83ecd4fc37891a1786083658dc457b2
Mihai Patru <mihaipatru05@gmail.com>
2025-03-25 05:04:47 +0200

    Add detailed simulation output for unused Animal, Habitat, Zoo, Staff, and Visitor instances in main.cpp

--- main.cpp
+++ main.cpp
@@ -1256,6 +1256,13 @@ int main()
     unusedAnimal.setPrice(2000);
     unusedAnimal.setHunger(0.5f);
     std::cout << "Unused Animal Name: " << unusedAnimal.getName() << std::endl;
+    std::cout << "Unused Animal Species: " << unusedAnimal.getSpecies() << std::endl;
+    std::cout << "Unused Animal Age: " << unusedAnimal.getAge() << std::endl;
+    std::cout << "Unused Animal Weight: " << unusedAnimal.getWeight() << std::endl;
+    std::cout << "Unused Animal Height: " << unusedAnimal.getHeight() << std::endl;
+    std::cout << "Unused Animal Health: " << unusedAnimal.getIsHealthy() << std::endl;
+    std::cout << "Unused Animal Price: " << unusedAnimal.getPrice() << std::endl;
+    std::cout << "Unused Animal Hunger: " << unusedAnimal.getHunger() << std::endl;
 
     Habitat unusedHabitat("Type", {}, 5, 0.5f, 5000.0f);
     unusedHabitat.setType("NewType");
@@ -1263,6 +1270,9 @@ int main()
     unusedHabitat.setCleanlinessLevel(0.75f);
     unusedHabitat.setPrice(7500.0f);
     std::cout << "Unused Habitat Type: " << unusedHabitat.getType() << std::endl;
+    std::cout << "Unused Habitat Capacity: " << unusedHabitat.getCapacity() << std::endl;
+    std::cout << "Unused Habitat Cleanliness: " << unusedHabitat.getCleanlinessLevel() << std::endl;
+    std::cout << "Unused Habitat Price: " << unusedHabitat.getPrice() << std::endl;
 
     Zoo unusedZoo("Name", {}, 0, false, 50000.0f);
     unusedZoo.setName("NewZooName");
@@ -1270,6 +1280,9 @@ int main()
     unusedZoo.setIsOpen(true);
     unusedZoo.setBuget(75000.0f);
     std::cout << "Unused Zoo Name: " << unusedZoo.getName() << std::endl;
+    std::cout << "Unused Zoo Visitor Count: " << unusedZoo.getVisitorCount() << std::endl;
+    std::cout << "Unused Zoo Is Open: " << unusedZoo.getIsOpen() << std::endl;
+    std::cout << "Unused Zoo Budget: " << unusedZoo.getBuget() << std::endl;
 
     Staff unusedStaff("Name", "Position", 30, 60000.0f);
     unusedStaff.setName("NewStaffName");
@@ -1277,6 +1290,9 @@ int main()
     unusedStaff.setAge(35);
     unusedStaff.setSalary(70000.0f);
     std::cout << "Unused Staff Name: " << unusedStaff.getName() << std::endl;
+    std::cout << "Unused Staff Position: " << unusedStaff.getPosition() << std::endl;
+    std::cout << "Unused Staff Age: " << unusedStaff.getAge() << std::endl;
+    std::cout << "Unused Staff Salary: " << unusedStaff.getSalary() << std::endl;
 
     Visitor unusedVisitor("Name", 20, 100.0f, "Type");
     unusedVisitor.setName("NewVisitorName");
@@ -1284,6 +1300,9 @@ int main()
     unusedVisitor.setMoney(150.0f);
     unusedVisitor.setType("NewType");
     std::cout << "Unused Visitor Name: " << unusedVisitor.getName() << std::endl;
+    std::cout << "Unused Visitor Age: " << unusedVisitor.getAge() << std::endl;
+    std::cout << "Unused Visitor Money: " << unusedVisitor.getMoney() << std::endl;
+    std::cout << "Unused Visitor Type: " << unusedVisitor.getType() << std::endl;
 
     return 0;
 }
